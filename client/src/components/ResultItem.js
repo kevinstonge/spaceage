c2ccb6c4e0ae18eb7c92c9c9359794656eacf747
Kevin St.Onge <kevinstonge@gmail.com>
2021-08-22 14:49:02 -0400

    addition of details element to condense displayed results

--- client/src/components/ResultItem.js
+++ client/src/components/ResultItem.js
@@ -5,14 +5,24 @@ export default function ResultItem(props) {
   if (typeof value === "object") {
     return (
       <div className="resultItem">
-      <p><span className="label">{property}</span>:</p>
-      {Object.entries(value).map((subProp,idx)=>{
-        const [subProperty,subValue]=subProp;
-        if (subValue === null) { return null }
-        return(
-          <ResultItem key={`${subProperty}-${idx}`} property={subProperty} value={subValue} />
-        )
-      })}
+        <details>
+          <summary>
+            <p>
+              <span className="label">
+                {property}
+                {value.name && ` (${value.name})`}
+                {!value.name && value.title && ` (${value.title})`}
+              </span>
+            </p>
+          </summary>
+          {Object.entries(value).map((subProp,idx)=>{
+            const [subProperty,subValue]=subProp;
+            if (subValue === null) { return null }
+            return(
+              <ResultItem key={`${subProperty}-${idx}`} property={subProperty} value={subValue} />
+            )
+        })}
+        </details>
       </div>
     )
   } else {
