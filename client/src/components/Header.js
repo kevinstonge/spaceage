02dfb79ed9e89868402a8c397a6e8a1e90ded966
Kevin St.Onge <kevinstonge@gmail.com>
2021-08-22 15:08:50 -0400

    added header image and added header sections

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,13 @@ import "../styles/Header.scss";
 const Header = () => {
   return (
     <header>
-      <h1>spaceage</h1>
+      <div className="left">
+        <img src={`${process.env.PUBLIC_URL}/logo70.png`} />
+        <h1>spaceage</h1>
+      </div>
+      <div className="right">
+        <p>right</p>
+      </div>
     </header>
   );
 };
